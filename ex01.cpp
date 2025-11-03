b75b8b6429d9260e710b67b39a303ef844d2b3e6
Witekx <witold.wesolowski@woli.edu.pl>
2025-11-03 09:41:34 +0000

    wynik zad1

---
+++ ex01.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x=11;
+    int*p = &x;
+    cout << p << endl;
+    cout << *p << endl;
+    x=*p=6;
+    cout << x << endl;
+    return 0;
+}
